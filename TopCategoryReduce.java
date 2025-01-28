a0d9b0055844c85445b325f7824ac749e73e989c
KhadijahRijal <s22b0008@siswa.umk.edu.my>
2025-01-28 22:57:07 +0800

    Add files via upload

---
+++ TopCategoryReduce.java
@@ -0,0 +1,25 @@
+package topcategorydata;
+
+import java.io.IOException;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+public class TopCategoryReduce extends Reducer<Text, Text,Text, LongWritable>{
+	public long max = -1;
+    public void reduce(Text key, Iterable <Text> values, Context context) 
+    		throws IOException, InterruptedException
+    {
+       max = 0;
+			
+       for (Text val : values)
+       {
+          String [] str = val.toString().split("\t");
+          if(str.length > 2)
+          max += Long.parseLong(str[5]);
+       }
+			
+       context.write(new Text(key), new LongWritable(max));
+    }
+}
